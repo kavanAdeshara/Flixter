a29e5eec1d3c0f6af3c84bb4ccef882fce3487b5
Kavan <kavanadeshara1@gmail.com>
2021-02-25 23:03:10 -0500

    updating

--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -5,22 +5,31 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double ratingBar;
+    int movieID;
+
+    // empty constructor for the Parceler library
+    public Movie(){}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         backdropPath = jsonObject.getString("backdrop_path");
+        ratingBar = jsonObject.getDouble("vote_average");
+        movieID = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -45,5 +54,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRatingBar() {
+        return ratingBar;
+    }
+
+    public int getMovieID() {
+        return movieID;
     }
 }
